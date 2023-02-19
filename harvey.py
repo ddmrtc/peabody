984133762e2d9b2a3506020e4c39cb6e7b1b8a00
dd <dd_mrtc@rambler.ru>
2023-02-19 19:55:28 +0300

    evening build

---
+++ harvey.py
@@ -0,0 +1,25 @@
+from common import *
+
+params = {
+    'name': 'harvey',
+    'prefix': '',
+    'debug': False,
+    }
+
+#### Что я тут делаю
+#   Обрабатываю данные
+#
+#
+#
+
+
+
+
+if __name__ == "__main__":
+    init(params)
+    logging.info('Started')
+
+    ticker = 'CVNA'
+
+    
+    
